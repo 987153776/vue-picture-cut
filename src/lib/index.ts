31eba1eeb0ee1ca419900da6191391783f869a08
光年以外 <987153776@qq.com>
2020-07-12 17:40:14 +0800

    新增工具类Utils，将各种操作进行二次封装，以便用户更灵活的自定义菜单。

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,26 +1,25 @@
 import {Vue} from "vue-property-decorator";
 import VuePictureCut from './views/vue-picture-cut.vue';
-import VuePictureCutCanvas from './views/vue-picture-cut-canvas.vue';
 import VuePictureCutMask from './views/vue-picture-cut-mask.vue';
 import VuePictureCutMenu from './views/vue-picture-cut-menu.vue';
 import Bezier from './views/Bezier';
 import createAnimation from './views/animation';
 import Tool from './views/tool';
+import createUtils from './views/Utils';
 
 export {
   VuePictureCut,
-  VuePictureCutCanvas,
   VuePictureCutMask,
   VuePictureCutMenu,
   Bezier,
   createAnimation,
-  Tool
+  Tool,
+  createUtils
 };
 
 export default {
   install: (Vue: Vue.VueConstructor): void => {
     Vue.component('vue-picture-cut', VuePictureCut);
-    Vue.component('vue-picture-cut-canvas', VuePictureCutCanvas);
     Vue.component('vue-picture-cut-mask', VuePictureCutMask);
     Vue.component('vue-picture-cut-menu', VuePictureCutMenu);
   }
---
+++ src/lib/views/Utils.ts
@@ -0,0 +1,120 @@
+import {ClipResult, CutInterface} from './interface';
+import PhotoRoot from './PhotoRoot';
+import PhotoMask from "@/lib/views/PhotoMask";
+import PhotoMain from "@/lib/views/PhotoMain";
+
+/**
+ * 二次包装工具类
+ * 简化api，方便自定义菜单等
+ */
+class Utils {
+
+  photoRoot: PhotoRoot;
+
+  constructor(cut: CutInterface) {
+    this.photoRoot = cut.photoRoot;
+  }
+
+  /**
+   * 裁剪
+   * @param maxPixel        最大像素
+   * @param encoderOptions  压缩率
+   * @param format          导出格式
+   */
+  cut(maxPixel?: number, encoderOptions?: number, format?: string): ClipResult | null{
+    if (!this.photoRoot) return null;
+    const mask = this.photoRoot.getEventList<PhotoMask>('PhotoMask');
+    if (mask) {
+      return mask.clip(maxPixel, encoderOptions, format);
+    }
+    return null;
+  }
+
+  /**
+   * 设置剪裁框
+   * @param w   比例宽
+   * @param h   比例高
+   */
+  setMaskSize(w: number, h: number): void {
+    if (!this.photoRoot) return;
+    const mask = this.photoRoot.getEventList<PhotoMask>('PhotoMask');
+    mask?.reset(w, h);
+  }
+
+  /**
+   * 按图片宽高比例设置剪裁框尺寸
+   */
+  setMaskSizeToOriginal (): void {
+    if (!this.photoRoot) return;
+    const main = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    if (main) {
+      this.setMaskSize(main.imgRect.w, main.imgRect.h)
+    }
+  }
+
+  /**
+   * 设置剪裁框是否可拖动改变大小
+   * @param resize
+   */
+  setMaskResize (resize = true): void {
+    if (!this.photoRoot) return;
+    const mask = this.photoRoot.getEventList<PhotoMask>('PhotoMask');
+    mask?.setResize(resize);
+  }
+
+  /**
+   * 图片旋转
+   * @param angle       逆时针角度
+   * @param animation   是否动画
+   */
+  rotate (angle: number, animation = false): void {
+    if (!this.photoRoot || angle % 360 === 0) return;
+    const main = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    main?.setAngle(main.showRect.r + angle, animation);
+  }
+
+  /**
+   * 设置图片垂直翻转
+   * @param animation   是否动画
+   */
+  setFlipV(animation?: boolean): void {
+    if (!this.photoRoot) return;
+    const main = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    main?.setFlipV(!main.showRect.sV, animation);
+  }
+
+  /**
+   * 设置图片水平翻转
+   * @param animation   是否动画
+   */
+  setFlipH(animation?: boolean): void {
+    if (!this.photoRoot) return;
+    const main = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    main?.setFlipH(!main.showRect.sH, animation);
+  }
+
+  /**
+   * 设置图片翻转
+   * @param sV          垂直
+   * @param sH          水平
+   * @param animation   是否动画
+   */
+  setFlip (sV: boolean, sH: boolean, animation?: boolean): void {
+    if (!this.photoRoot) return;
+    const main = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    main?.setFlip(!main.showRect.sV, !main.showRect.sH, animation);
+  }
+
+  /**
+   * 图片缩放
+   * @param zoom    缩放系数
+   */
+  scale(zoom: number): void{
+    const photoMain = this.photoRoot.getEventList<PhotoMain>('PhotoMain');
+    photoMain?.scale(zoom);
+  }
+}
+
+export default function createUtils(cut: CutInterface): Utils {
+  return new Utils(cut);
+}
