b676f678366b39488c8b34e91f52c1d692d9ccbb
光年以外 <987153776@qq.com>
2025-09-18 14:07:23 +0800

    3.x

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  outputDir: 'dist',
+  indexPath: 'index.html',
+  filenameHashing: false,
+  productionSourceMap: false
 })
