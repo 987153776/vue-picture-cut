c5c07145967518d98887bce3cc12b28e1a72b060
光年以外 <987153776@qq.com>
2025-10-30 21:52:59 +0800

    ## 3.0.0-alpha.2 (2025-10-30)
    #### :bug: Bug Fix
    * 修改babel配置，兼容低版本浏览器。

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/cli-plugin-babel/preset',
+      {
+        targets: {
+          ie: '11',
+          browsers: ['> 0.2%', 'not dead']
+        },
+        // 不将 ES6 模块语法转换为 CommonJS（留给 Webpack 处理）
+        modules: false,
+        // 使用 core-js 进行 polyfill
+        useBuiltIns: "usage", // 按需注入 polyfill
+        corejs: {
+          version: 3, // 使用 core-js@3
+          proposals: true,  // 启用提案阶段的 polyfill
+        },
+      }
+    ]
   ]
 }
